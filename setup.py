e62ef01a138b355f128bbd849c6d7d98f637b6b9
Johann Bich <johann.bich@axa.co.jp>
2019-02-19 19:45:28 +0900

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="event-gateway-sdk",
-    version="1.0.0",
+    version="1.1.0",
     author="Johann BICH",
     author_email="johannbich@gmail.com",
     description="Event Gateway Python SDK",
